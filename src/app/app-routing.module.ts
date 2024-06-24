e7d3829c67a0efc0d217abda3855fc637892befe
RuddyQuispe <ruddy.bryan.ts@gmail.com>
2024-06-24 03:10:33 -0400

    listado y detalle de tickets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TicketsPageComponent } from './pages/tickets-page/tickets-page.component';
+import { TicketsDetailPageComponent } from './pages/tickets-detail-page/tickets-detail-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  {
+    path: 'tickets',
+    children: [
+      { path: '', component: TicketsPageComponent },
+      { path: ':ticket_id', component: TicketsDetailPageComponent }
+    ]
+  }
+  /* {
+    path: 'internal',
+    canActivate: [],
+    children: [
+      { path: 'result', component: ResultComponent },
+      { path: 'institution', component: InstitutionComponent },
+      { path: 'admin', component: MenuAdministrationComponent },
+      { path: 'admin-history', component: AdmHistoryComponent },
+      { path: 'admin-profile', component: AdmProfileComponent },
+      { path: 'admin-report', component: AdmEvolutionaryReportComponent },
+    ]
+  } */
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/template/menu/menu.component';
+import { TicketsPageComponent } from './pages/tickets-page/tickets-page.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TicketsDetailPageComponent } from './pages/tickets-detail-page/tickets-detail-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    TicketsPageComponent,
+    TicketsDetailPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
